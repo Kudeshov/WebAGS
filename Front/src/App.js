cf0041949b21e1409d3e6de872a53f7e5b51680a
Evgeny Kudeshov <eug@postmodern.ru>
2023-10-11 12:37:48 +0300

    My modifications

--- Front/src/App.js
+++ Front/src/App.js
@@ -8,7 +8,7 @@ function App() {
   return (
     <div className="App">
       <h1>Веб Аэрогаммасъемка</h1>
-      <MyChartComponent />
+      {/* <MyChartComponent /> */}
       <MyMapComponent />
     </div>
   );
