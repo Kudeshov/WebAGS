09f4315e86220ac469a40311d375c799bfbd13ef
Fedor <brinoher@gmail.com>
2023-10-11 12:11:25 +0300

    фксрцфму

--- Front/src/App.js
+++ Front/src/App.js
@@ -1,17 +1,28 @@
-//import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Grid } from '@mui/material';
 import MyDataGrid from './MyDataGrid';
 import MyMapComponent from './MyMapComponent';
 import MyChartComponent from './MyChartComponent';
-
 
 function App() {
   return (
     <div className="App">
       <h1>Веб Аэрогаммасъемка</h1>
-      <MyChartComponent />
-      <MyMapComponent />
-      <MyDataGrid />
+      <Grid container spacing={2}>
+        <Grid item xs={12} sm={6}>
+          <MyMapComponent />
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <Grid container direction="column">
+            <Grid item>
+              <MyChartComponent />
+            </Grid>
+            <Grid item>
+              <MyDataGrid />
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
     </div>
   );
 }
--- Front/src/MyChartComponent.js
+++ Front/src/MyChartComponent.js
@@ -1,8 +1,10 @@
-import React, { useEffect, useState } from 'react';
-import { LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip } from 'recharts';
+import React, { useEffect, useState, useRef } from 'react';
+import * as d3 from 'd3';
 
 function MyChartComponent() {
   const [data, setData] = useState([]);
+
+  const svgRef = useRef();
 
   useEffect(() => {
     fetch("http://localhost:3001/api/data")
@@ -17,17 +19,54 @@ function MyChartComponent() {
       });
   }, []);
 
+  useEffect(() => {
+    if (data.length > 0) {
+      const margin = { top: 20, right: 30, bottom: 30, left: 40 };
+      const width = 800 - margin.left - margin.right;
+      const height = 400 - margin.top - margin.bottom;
+
+      const svg = d3.select(svgRef.current)
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform", `translate(${margin.left},${margin.top})`);
+
+      const x = d3.scaleBand()
+        .domain(data.map(d => d.datetime))
+        .range([0, width])
+        .padding(0.1);
+
+      const y = d3.scaleLinear()
+        .domain([0, d3.max(data, d => d.spectrumValue)])
+        .nice()
+        .range([height, 0]);
+
+      svg.append("g")
+        .attr("class", "x-axis")
+        .attr("transform", `translate(0,${height})`)
+        .call(d3.axisBottom(x));
+
+      svg.append("g")
+        .attr("class", "y-axis")
+        .call(d3.axisLeft(y));
+
+      svg.selectAll(".bar")
+        .data(data)
+        .enter().append("rect")
+        .attr("class", "bar")
+        .attr("x", d => x(d.datetime))
+        .attr("y", d => y(d.spectrumValue))
+        .attr("width", x.bandwidth())
+        .attr("height", d => height - y(d.spectrumValue))
+        .attr("fill", "#8884d8");
+    }
+  }, [data]);
+
   return (
     <div style={{ width: '100%', height: '400px' }}>
-      <LineChart width={800} height={400} data={data}>
-        <Line type="monotone" dataKey="spectrumValue" stroke="#8884d8" />
-        <CartesianGrid stroke="#ccc" />
-        <XAxis dataKey="datetime" />
-        <YAxis />
-        <Tooltip />
-      </LineChart>
+      <svg ref={svgRef}></svg>
     </div>
   );
 }
 
-export default MyChartComponent;
+export default MyChartComponent; 
