06f0468cbaf4ba66e92abd6c5ca704c9c66baf4e
Fedor <brinoher@gmail.com>
2024-06-25 15:28:57 +0300

    конфиг дерьмо, поправил файл node_modules/react-scripts/config/webpackDevServer.config.js: allowedHosts: disableFirewall ? 'all' : ['localhost'],

--- Front/webpack.config.js
+++ Front/webpack.config.js
@@ -1,11 +1,5 @@
 module.exports = {
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        enforce: "pre",
-        use: ["source-map-loader"],
-      },
-    ],
+  devServer: {
+    allowedHosts: 'all',
   },
 };
